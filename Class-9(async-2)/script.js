93968370d3934a2ca75de018c23910ade0f72395
mrinal1224 <iammrinalkc@gmail.com>
2026-03-24 15:22:48 +0530

    push

---
+++ Class-9(async-2)/script.js
@@ -0,0 +1,27 @@
+// Promise Combinators
+// all , allSettled , race , any
+
+function fetchUserData() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve({ userId: 1, username: "JohnDoe" }), 1000);
+  });
+}
+
+function fetchUserPosts(data) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(["Post 1", "Post 2", "Post 3"]), 1000);
+  });
+}
+
+function fetchUserComments() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let success = Math.random() > 0; // 50% chance of failure
+      if (success) {
+        resolve(["Nice!", "Interesting post", "Subscribed!"]);
+      } else {
+        reject("Failed to fetch comments ❌");
+      }
+    }, 800);
+  });
+}
